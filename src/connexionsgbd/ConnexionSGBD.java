8e4ef4a6ea46fe0ad9b7fef22478ab21f7afd2fc
Pierre Belabbes <pbelabbes@gmail.com>
2018-04-06 00:02:07 +0200

    Add connection package

---
+++ src/connexionsgbd/ConnexionSGBD.java
@@ -0,0 +1,42 @@
+package connexionsgbd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class ConnexionSGBD {
+	
+	private static final String configurationFile = "BD.properties";
+	
+	public static void main(String args[]){
+		
+		try {
+			String jdbcDriver, dbUrl, username, password;
+			
+			DatabaseAccessProperties dap = new DatabaseAccessProperties(configurationFile);
+			jdbcDriver = dap.getJdbcDriver();
+			dbUrl = dap.getDatabaseUrl();
+			username = dap.getUsername();
+			password = dap.getPassword();
+			
+			// Load the database driver
+			Class.forName(jdbcDriver);
+			// Get a connection to the database
+			System.out.println(username + password);
+			Connection conn = DriverManager.getConnection(dbUrl, username, password);
+			// Print information about connection warnings
+			SQLWarningsExceptions.printWarnings(conn);
+			conn.close();
+		}
+		catch( SQLException se ) {
+			// Print information about SQL exceptions
+			SQLWarningsExceptions.printExceptions(se);
+			return;
+		}
+		catch( Exception e ) {
+			System.err.println( "Exception: " + e.getMessage()) ;
+			e.printStackTrace();
+			return;
+		}
+	}
+}
